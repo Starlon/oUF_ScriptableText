f8a28c77e70e03d0e8e78bafdfe67ce3ec18091b
Scott Sibley <sisibley@gmail.com>
2011-08-05 21:51:47 -0500

    Beyond dry code. ;)

--- oUF_ScriptableText.lua
+++ oUF_ScriptableText.lua
@@ -2,18 +2,27 @@ local parent, ns = ...
 local oUF = oUF
 local LibCore = assert(LibStub("LibScriptableLCDCoreLite-1.0"))
 local WidgetText = assert(LibStub("LibScriptableWidgetText-1.0"))
-local core = LibCore:New({}, "oUF_ScriptableText")
+local core = LibCore:New({}, "oUF_ScriptableText", 1)
 
-local Update = function(self)
-	local fontstring = self.widget.fontstring
-	fontstring:SetText(self.widget.buffer or "")
+local Update = function(self, fs)
+	if fs and fs:GetObjectType() == "FontString" then
+		fs:SetText(self.buffer)
+	end
 end
 
 local Enable = function(self, unit)
-	self.widget = self.widget or WidgetText:New(core, self.name, {value=self.value, update=self.update, repeating=self.repeating, speed=self.speed, direction=self.direction, align=self.align, cols=self.cols, prefix=self.prefix, postfix=self.postfix}, row, col, layer, errorLevel, Update, self)
-	self.widget.fontstring = self.fontstring
-	self.widget.environment.unit = unit
-	self.widget:Start()
+	self.value = "return 'lolollll'"
+	local text = self.ScriptableText
+	local col, row, layer = 0, 0, 0
+	local errorLevel = 2
+	if text and text:GetObjectType() == "FontString" then
+		self.widget = self.widget or WidgetText:New(core, self.name or "ScriptableText", {value=self.value, update=100, repeating=self.repeating, speed=self.speed, direction=self.direction, align=self.align, cols=self.cols, prefix=self.prefix, postfix=self.postfix}, row, col, layer, errorLevel, Update, text)
+		self.widget.environment.unit = unit
+		self.widget:Start()
+		text.__owner = self
+		text.__widget = self.widget
+		self.widget.fontstring = text
+	end
 	return true
 end
 
